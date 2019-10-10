2c65769223ff8cf10833d52494bc72a6d0962d62
Brandon Trabucco <brandon@btrabucco.com>
2019-10-10 04:38:16 -0700

    changed handling of leaf node base case

--- best_first/orderings/ordering.py
+++ best_first/orderings/ordering.py
@@ -33,7 +33,7 @@ class Ordering(ABC):
             left_words=candidate_words,
             left_tags=candidate_tags,
             slots=candidate_slots,
-            violations=order_violations)
+            violations=order_violations) if split_words.size > 0 else []
         return orderings + [Insertion(
             words=np.concatenate([[2], split_words, [3]]),
             tags=np.concatenate([[1], split_tags, [1]]),
@@ -52,23 +52,22 @@ class Ordering(ABC):
             violations=0
     ):
         orderings = []
-        if words.size > 0:
-            removal_index = np.argmin(self.score(words, tags))
-            for i, word, tag in enumerate(zip(words, tags)):
-                order_violations = violations if i == removal_index else violations + 1
-                if order_violations <= self.max_violations:
-                    split_words = np.append(words[:i], words[i + 1:])
-                    split_tags = np.append(tags[:i], tags[i + 1:])
-                    candidate_words = np.append([word], left_words)
-                    candidate_tags = np.append([tag], left_tags)
-                    candidate_slots = np.append([i], np.where(i < slots, slots - 1, slots))
-                    orderings.extend(self.get_orderings(
-                        split_words,
-                        split_tags,
-                        candidate_words,
-                        candidate_tags,
-                        candidate_slots,
-                        order_violations))
+        removal_index = np.argmin(self.score(words, tags))
+        for i, word, tag in enumerate(zip(words, tags)):
+            order_violations = violations if i == removal_index else violations + 1
+            if order_violations <= self.max_violations:
+                split_words = np.append(words[:i], words[i + 1:])
+                split_tags = np.append(tags[:i], tags[i + 1:])
+                candidate_words = np.append([word], left_words)
+                candidate_tags = np.append([tag], left_tags)
+                candidate_slots = np.append([i], np.where(i < slots, slots - 1, slots))
+                orderings.extend(self.get_orderings(
+                    split_words,
+                    split_tags,
+                    candidate_words,
+                    candidate_tags,
+                    candidate_slots,
+                    order_violations))
         return orderings
 
     @abstractmethod
