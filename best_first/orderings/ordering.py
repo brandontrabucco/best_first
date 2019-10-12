cd423f2cfcb8e91aa712efb6ecc024b7a18eeaa3
Brandon Trabucco <brandon@btrabucco.com>
2019-10-11 19:27:33 -0700

    fixed ordering of forward ordering

--- best_first/orderings/ordering.py
+++ best_first/orderings/ordering.py
@@ -21,53 +21,61 @@ class Ordering(ABC):
             candidate_words,
             candidate_tags,
             candidate_slots,
-            order_violations
+            order_violations,
+            closed
     ):
-        insertion_index = np.argmax(self.score(candidate_words, candidate_tags))
-        insertion_word = candidate_words[insertion_index]
-        insertion_tag = candidate_tags[insertion_index]
-        insertion_slot = candidate_slots[insertion_index]
         orderings = self.expand(
             split_words,
             split_tags,
             left_words=candidate_words,
             left_tags=candidate_tags,
             slots=candidate_slots,
-            violations=order_violations) if split_words.size > 0 else []
+            violations=order_violations,
+            closed=closed) if split_words.size > 0 else []
+
+        insertion_index = np.argmax(self.score(candidate_words, candidate_tags))
         return orderings + [Insertion(
             words=np.concatenate([[2], split_words, [3]]),
             tags=np.concatenate([[1], split_tags, [1]]),
-            next_word=insertion_word,
-            next_tag=insertion_tag,
-            slot=insertion_slot,
+            next_word=candidate_words[insertion_index],
+            next_tag=candidate_tags[insertion_index],
+            slot=candidate_slots[insertion_index],
             violations=order_violations)]
 
     def expand(
             self,
             words,
             tags,
-            left_words=np.zeros([], dtype=np.int32),
-            left_tags=np.zeros([], dtype=np.int32),
-            slots=np.zeros([], dtype=np.int32),
-            violations=0
+            left_words=np.zeros([0], dtype=np.int32),
+            left_tags=np.zeros([0], dtype=np.int32),
+            slots=np.zeros([0], dtype=np.int32),
+            violations=0,
+            closed=None
     ):
         orderings = []
+        closed = closed if closed is not None else set()
         removal_index = np.argmin(self.score(words, tags))
-        for i, word, tag in enumerate(zip(words, tags)):
+        for i, (word, tag) in enumerate(zip(words, tags)):
             order_violations = violations if i == removal_index else violations + 1
             if order_violations <= self.max_violations:
+
                 split_words = np.append(words[:i], words[i + 1:])
-                split_tags = np.append(tags[:i], tags[i + 1:])
-                candidate_words = np.append([word], left_words)
-                candidate_tags = np.append([tag], left_tags)
-                candidate_slots = np.append([i], np.where(i < slots, slots - 1, slots))
-                orderings.extend(self.get_orderings(
-                    split_words,
-                    split_tags,
-                    candidate_words,
-                    candidate_tags,
-                    candidate_slots,
-                    order_violations))
+                if split_words.tostring() not in closed:
+                    closed.add(split_words.tostring())
+
+                    split = 0
+                    while split < slots.size and slots[split] <= i:
+                        split += 1
+
+                    orderings.extend(self.get_orderings(
+                        split_words,
+                        np.append(tags[:i], tags[i + 1:]),
+                        np.concatenate([left_words[:split], [word], left_words[split:]]),
+                        np.concatenate([left_tags[:split], [tag], left_tags[split:]]),
+                        np.concatenate([slots[:split], [i], slots[split:] + 1]),
+                        order_violations,
+                        closed))
+
         return orderings
 
     @abstractmethod
